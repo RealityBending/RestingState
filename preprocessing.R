6ebe6644250c43de1ac55c8111a76abac270880e
Dominique Makowski <dom.mak19@gmail.com>
2022-07-05 12:25:01 +0800

    update

--- preprocessing.R
+++ preprocessing.R
@@ -1,9 +1,50 @@
-# install.packages("jsonlite")
+# Preprocessing function
+preprocess_RS <- function(file) {
+  if (!require("jsonlite", character.only = TRUE)) install.packages("jsonlite")
 
-files <- list.files(path="data/", pattern = "\\.json$", full.names = TRUE)
+  items <- c(    "I had busy thoughts",
+                 "I had rapidly switching thoughts",
+                 "I had difficulty holding onto my thoughts",
+                 "I thought about others",
+                 "I thought about people I like",
+                 "I placed myself in other people's shoes",
+                 "I thought about my feelings",
+                 "I thought about my behaviour",
+                 "I thought about myself",
+                 "I thought about things I need to do",
+                 "I thought about solving problems",
+                 "I thought about the future",
+                 "I felt sleepy",
+                 "I felt tired",
+                 "I had difficulty staying awake",
+                 "I felt comfortable",
+                 "I felt happy",
+                 "I felt relaxed",
+                 "I was conscious of my body",
+                 "I thought about my heartbeat",
+                 "I thought about my breathing",
+                 "I felt ill",
+                 "I thought about my health",
+                 "I felt pain",
+                 "I thought in images",
+                 "I pictured events",
+                 "I pictured places",
+                 "I thought in words",
+                 "I had silent conversations",
+                 "I imagined talking to myself",
+                 "I had my eyes closed", "I was able to rate the statements above")
 
-df <- data.frame()
-for(file in files) {
+  names(items) <- c(    "DoM_1", "DoM_2", "DoM_3",
+                        "ToM_1", "ToM_2", "ToM_3",
+                        "Self_1", "Self_2", "Self_3",
+                        "Plan_1", "Plan_2", "Plan_3",
+                        "Sleep_1", "Sleep_2", "Sleep_3",
+                        "Comfort_1", "Comfort_2", "Comfort_3",
+                        "SomA_1", "SomA_2", "SomA_3",
+                        "Health_1", "Health_2", "Health_3",
+                        "Visual_1", "Visual_2", "Visual_3",
+                        "Verbal_1", "Verbal_2", "Verbal_3", "Check_1", "Check_2")
+
   json <- jsonlite::fromJSON(file, simplifyVector=FALSE)[[1]]
   data <- data.frame(Participant = json$participant_id,
                      Duration = json$duration,
@@ -13,7 +54,25 @@ for(file in files) {
                      Item = names(unlist(json$response)),
                      Answer = as.numeric(unlist(json$response)),
                      Order = unlist(json$question_order))
-  df <- rbind(df, data)
+  data$Label <- items[data$Item]
+
+  # Average per dimension
+  data$Dimension <- sapply(strsplit(data$Item, "_", fixed=TRUE), function(x) x[[1]])
+  ave <- aggregate(data$Answer, list(data$Dimension), FUN=mean)
+  ave <- setNames(ave$x, ave$Group.1)
+  data <- cbind(data, as.data.frame(as.list(ave)))
+  data
 }
 
-data
+
+
+# Run ---------------------------------------------------------------------
+
+files <- list.files(path="data/", pattern = "\\.json$", full.names = TRUE)
+
+# Loop over each file and compute function
+df <- data.frame()
+for(file in files) df <- rbind(df, preprocess_RS(file))
+
+# Clean dataframe
+df
