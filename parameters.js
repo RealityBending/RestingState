f3f20ddfb74b17489b5c55879192b35d3632d706
An Shu Te <teanshu97@gmail.com>
2022-11-03 11:21:08 +0800

    widen marker

--- parameters.js
+++ parameters.js
@@ -9,7 +9,7 @@ version = "1.0"
 // Resting state duration in min
 duration = 8
 // [x, y, width, height] in pixels. Set to [0, 0, 0, 0] to disable.
-marker_position = [0, 0, 100, 100]
+marker_position = [0, 0, 200, 100]
 // Are all the questions from the debriefing required?
 questions_required = false
 // Record webcam?
